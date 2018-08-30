18c3fd7d9ecbc6bc049f7e19dc00cf8d747f69a8
Administrator <Administrator@USER-20150330FB>
2018-08-31 00:14:28 +0800

    更改基础框架

---
+++ Component.Infrastructure/Log/LogExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Component.Infrastructure.Log
+{
+    public static class LogExtension
+    {
+        private ILog logger;
+        public Log(ILog log)
+        {
+            this.logger = log;
+        }
+        public void Debug(object message)
+        {
+            this.logger.Debug(message);
+        }
+        public void Error(object message)
+        {
+            this.logger.Error(message);
+        }
+        public void Info(object message)
+        {
+            this.logger.Info(message);
+        }
+        public void Warn(object message)
+        {
+            this.logger.Warn(message);
+        }
+    }
+}
