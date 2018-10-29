a7bb31c8a57be7ad1f331ae91f41216ad9ab02b9
Administrator <Administrator@USER-20150330FB>
2018-10-29 21:10:03 +0800

    添加图片缩放

--- Component.Infrastructure/File/ThumbnailHelper.cs
+++ Component.Infrastructure/File/ThumbnailHelper.cs
@@ -118,5 +118,107 @@ namespace Component.Infrastructure
         {
             MakeThumbnail(originalImagePath, thumbnailPath, 140, 140, "HW");
         }
+
+        /// <summary>
+        /// 生成缩略图
+        /// </summary>
+        /// <param name="oraginalimg">原图数组</param>
+        /// <param name="width">缩略图宽</param>
+        /// <param name="height">缩略图高</param>
+        /// <param name="mode">哪种方式生成缩略图，默认为百分比压缩</param>
+        /// <returns></returns>
+        public static byte[] ResizeImageBytesToBytes(byte[] oraginalimg, int width, int height, string mode = "DB")
+        {
+            byte[] s = null;
+            MemoryStream ms = new MemoryStream(oraginalimg);
+            Bitmap bm = new Bitmap(ms);
+            //System.Drawing.Image originalImage = System.Drawing.Image.FromFile(originalImagePath);
+            int towidth = width;
+            int toheight = height;
+            int x = 0;
+            int y = 0;
+            int ow = bm.Width;
+            int oh = bm.Height;
+            switch (mode)
+            {
+                case "HW"://指定高宽缩放（可能变形） 
+                    break;
+                case "W"://指定宽，高按比例 
+                    toheight = bm.Height * width / bm.Width;
+                    break;
+                case "H"://指定高，宽按比例
+                    towidth = bm.Width * height / bm.Height;
+                    break;
+                case "Cut"://指定高宽裁减（不变形） 
+                    if ((double)bm.Width / (double)bm.Height > (double)towidth / (double)toheight)
+                    {
+                        oh = bm.Height;
+                        ow = bm.Height * towidth / toheight;
+                        y = 0;
+                        x = (bm.Width - ow) / 2;
+                    }
+                    else
+                    {
+                        ow = bm.Width;
+                        oh = bm.Width * height / towidth;
+                        x = 0;
+                        y = (bm.Height - oh) / 2;
+                    }
+                    break;
+                case "DB"://等比缩放（不变形，如果高大按高，宽大按宽缩放） 
+                    if ((double)bm.Width / (double)towidth < (double)bm.Height / (double)toheight)
+                    {
+                        toheight = height;
+                        towidth = bm.Width * height / bm.Height;
+                    }
+                    else
+                    {
+                        towidth = width;
+                        toheight = bm.Height * width / bm.Width;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            try
+            {
+                //新建一个bmp图片
+                System.Drawing.Image bitmap = new System.Drawing.Bitmap(towidth, toheight);
+                //新建一个画板
+                System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bitmap);
+                //设置高质量插值法
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                //设置高质量,低速度呈现平滑程度
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                //清空画布并以透明背景色填充
+                g.Clear(System.Drawing.Color.Transparent);
+                //在指定位置并且按指定大小绘制原图片的指定部分
+                g.DrawImage(bm, new System.Drawing.Rectangle(0, 0, towidth, toheight),
+                new System.Drawing.Rectangle(x, y, ow, oh),
+                System.Drawing.GraphicsUnit.Pixel);
+
+                //把图片转byte数组
+                MemoryStream ms1 = new MemoryStream();
+                bitmap.Save(ms1, System.Drawing.Imaging.ImageFormat.Jpeg);
+                s = ms1.ToArray();
+                bm.Dispose();
+                bitmap.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return s;
+        }
+
+        public static byte[] ResizeImage(Stream fileStream, int width, int height, string mode = "DB")
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                fileStream.Position = 0;
+                fileStream.CopyTo(ms);
+                return ResizeImageBytesToBytes(ms.ToArray(), width, height, mode); ;
+            }
+        }
     }
 }
