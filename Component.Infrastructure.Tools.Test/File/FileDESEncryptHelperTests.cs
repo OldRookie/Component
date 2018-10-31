717bbbb7fa11c8701ac69ef43a58e1c08f6b9652
Administrator <Administrator@USER-20150330FB>
2018-10-31 20:32:19 +0800

    修改加密方法

---
+++ Component.Infrastructure.Tools.Test/File/FileDESEncryptHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Component.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Component.Infrastructure.Tests
+{
+    [TestClass()]
+    public class FileDESEncryptHelperTests
+    {
+        string dir = @"D:\Project\SourceCode\DotNet\Framework";
+
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            using (FileStream in_stream = new FileStream(Path.Combine( dir,"Job.7z"),
+               FileMode.Open, FileAccess.Read))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    //ms.Position = 0;
+                    in_stream.CopyTo(ms);
+                    var cryptBytes = FileDESEncryptHelper.Encrypt(ms.ToArray(), "...");
+                    File.WriteAllBytes(Path.Combine(dir, "Job_en.7z"), cryptBytes);
+                }
+
+            }
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            using (FileStream in_stream = new FileStream(Path.Combine(dir, "Job_en.7z"),
+               FileMode.Open, FileAccess.Read))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    //ms.Position = 0;
+                    in_stream.CopyTo(ms);
+                    var cryptBytes = FileDESEncryptHelper.Decrypt(ms.ToArray(), "...");
+                    File.WriteAllBytes(Path.Combine(dir, "Job_de.7z"), cryptBytes);
+                }
+            }
+        }
+    }
+}
--- Component.Infrastructure/File/FileDESEncryptHelper.cs
+++ Component.Infrastructure/File/FileDESEncryptHelper.cs
@@ -38,7 +38,8 @@ namespace Component.Infrastructure
             CryptoStream crypto_stream = new CryptoStream(out_stream, crypto_transform, CryptoStreamMode.Write);
             StreamWriter streamWriter = new StreamWriter(crypto_stream);
             crypto_stream.Write(in_date, 0, in_date.Length);
-            b = out_stream.ToArray();
+            //b = out_stream.ToArray();
+            b = crypto_transform.TransformFinalBlock(in_date, 0, in_date.Length);
 
             return b;
         }
@@ -137,6 +138,75 @@ namespace Component.Infrastructure
                 // return b_emptyp;
             }
             return b_emptyp;
+        }
+
+        public static byte[] Encrypt(byte[] source, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = source;
+
+            var encoded = desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length);
+            return encoded;
+        }
+
+        public static byte[] Decrypt(byte[] encodedBytes, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = encodedBytes;
+
+            var plaintext = desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length);
+            return plaintext;
+        }
+
+        public string Encrypt(string source, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Encoding.UTF8.GetBytes(source);
+
+            string encoded =
+                Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return encoded;
+        }
+
+        public static string Decrypt(string encodedText, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Convert.FromBase64String(encodedText);
+
+            string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return plaintext;
         }
     }
 
